b3246a748ab2d5b4ef06993158f7a9266e081d23
Zeynab <zeynab@MacBook-Air-Zeynab.local>
2023-02-14 11:48:31 +0400

    end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+
+      let str = e.target.value.replace(/\D/g, "");
+      if (str.length === 0) {
+          setInputValue(" ")
+      }
+      else if (str.length <= 3 ) {
+          setInputValue("(" + str)
+      } else if (str.length <= 6) {
+          setInputValue("(" + str.slice(0, 3) + ")" + str.slice(3, 6))
+      } else if (str.length <= 10) {
+          setInputValue("(" + str.slice(0, 3) + ")" + str.slice(3, 6) + "-" + str.slice(6, 10))
+      }
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <input onChange={handleChange} type="text" value={inputValue} placeholder="(555)555-5555"/>
+          <button disabled={inputValue.length!==13} type="submit">Submit</button>
+      </>
   );
 }
 
